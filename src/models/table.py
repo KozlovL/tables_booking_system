502639e3006fdf385a4cfa0f1107d3ef0754c661
Александр Спиридонов <spiridonov.a.e.13@gmail.com>
2025-09-22 20:01:32 +0300

    fix model

--- src/models/table.py
+++ src/models/table.py
@@ -1,14 +1,11 @@
-from sqlalchemy import Column, Integer, Boolean, ForeignKey, Text
+from sqlalchemy import Column, Integer, ForeignKey, Text
 from sqlalchemy.orm import relationship
-from src.core.db import Base 
+from src.core.db import Base
 
 
 class TableModel(Base):
     __tablename__ = "tables"
 
-    id = Column(Integer, primary_key=True, index=True)
-    description = Column(Text)
+    description = Column(Text, nullable=True)
     seats_number = Column(Integer, nullable=False)
-    seats = Column(Integer, nullable=False)
-    acive = Column(Boolean, default=True)
     cafe = Column(Integer, ForeignKey("cafes.id"), nullable=False)
